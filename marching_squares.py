65932231b1d818c062124e7f67794279fb35f7ba
TristanAntonsen <tristan.antonsen@gmail.com>
2023-10-19 22:09:54 -0500

    starting a more refined .py file

--- marching_squares.py
+++ marching_squares.py
@@ -1,6 +1,7 @@
 import numpy as np
-from cells import CELL_TYPES
-
+from PIL import Image, ImageDraw
+import numpy as np
+from tables import STATES
 
 class Grid:
 
@@ -19,55 +20,146 @@ class Grid:
         
         self.points = np.array(points)
 
-class UnitCell:
+def get_state(a,b,c,d,threshold):
+    
+    # Either 0 or 1
+    # < threshold = 0
+    # > threshold = 1
 
-    def __init__(self, type):
+    a = threshold + a > threshold
+    b = threshold + b > threshold
+    c = threshold + c > threshold
+    d = threshold + d > threshold
+
+    # Index for lookup table
+    s = round(a * 8 + b * 4 + c * 2 + d * 1)
+
+    return s
+
+def circle(x, y, cx, cy, radius):
+    # distance from center point
+    d = np.sqrt((cx-x) ** 2 + (cy-y) ** 2) - radius
+    return d
+
+def lerp(a, b, t):
+    c = a + t * (b - a)
+    return c
+
+def lerp_points(p0, p1, t):
+
+    return [
+        lerp(p0[0], p1[0], t),
+        lerp(p0[1], p1[1], t)
+    ]
+
+def find_lerp_factor(v0, v1, iso_val):
+    return (iso_val - v0) / (v1 - v0)
+
+def map(x, y):
+    d = circle(x, y, 540, 540, 400)
+    return d
+
+def march(grid, draw_points, iso, interpolated=True):
+
+    graph = []
+
+    for point in grid.points:
+
+        scale = grid.scale
+
+        x = point[0]
+        y = point[1]
+
+        ## Corner points
+        p0 = (x        , y        )
+        p1 = (x + scale, y        )
+        p2 = (x + scale, y + scale)
+        p3 = (x        , y + scale)
+
+        ## values at (corner) points
+        v0 = map(*p0)
+        v1 = map(*p1)
+        v2 = map(*p2)
+        v3 = map(*p3)
+
+        corner_values = [v0, v1, v2, v3]
+
+        ## Interopolation factors
+        ta = find_lerp_factor(v0, v1, iso)
+        tb = find_lerp_factor(v1, v2, iso)
+        tc = find_lerp_factor(v3, v2, iso) # flip due to sign change
+        td = find_lerp_factor(v0, v3, iso)
+
+        ## edge point locations (interpolated)
+        a = [x + ta * scale , y              ] # 01
+        b = [x + scale      , y + tb * scale ] # 12
+        c = [x + tc * scale , y + scale      ] # 23
+        d = [x              , y + td * scale ] # 30
+
+        ## edge point locations (not interpolated)
+        _a = [x + 0.5 * scale , y               ] # 01
+        _b = [x + scale       , y + 0.5 * scale ] # 12
+        _c = [x + 0.5 * scale , y + scale       ] # 23
+        _d = [x               , y + 0.5 * scale ] # 30
+
+        edge_points = [a,b,c,d]
+
+        if interpolated == False:
+            edge_points = [_a,_b,_c,_d]
+
+        state = get_state(v0, v1, v2, v3, 0)
+        edges = STATES[state]
         
-        self.nodes = CELL_TYPES[type]['nodes']
-        self.beams = CELL_TYPES[type]['beams']
-        self.x_scale = 10
-        self.y_scale = 10
 
-class Lattice:
+        for line in edges:
+            p1 = edge_points[line[0]]
+            p2 = edge_points[line[1]]
+            graph.append((p1, p2))
 
-    def __init__(self, unit_cell, x_count, y_count):
-        self.unit_cell = unit_cell
-        self.cell_x_scale = unit_cell.x_scale
-        self.cell_y_scale = unit_cell.y_scale
-        self.x_count = x_count
-        self.y_count = y_count
+    return graph
 
-        # self.return_cell(1,1)
+def main():
 
-    def return_cell_coords(self, x, y):
+    image_resolution = 1080
+    img = Image.new('RGB', (image_resolution, image_resolution))
+    draw = ImageDraw.Draw(img)
 
-        if x > self.x_count or y > self.y_count:
-            print("Index too large")
-            return
+    def center_ellipse(x,y,r,c):
 
-        node_coords = self.unit_cell.nodes.copy()
+        draw.ellipse([x - r, y - r, x + r, y + r], fill=c)
 
-        for node in node_coords:
-            node[0] = node[0] * self.cell_x_scale + self.cell_x_scale * x
-            node[1] = node[1] * self.cell_y_scale + self.cell_y_scale * y
+    def center_rectangle(x,y,l,w,c):
+        l = l/2
+        w = w/2
+        draw.rectangle([x - w, y - l, x + w, y + l], fill=c)
 
-        return node_coords
+    radius = 400
+    interpolated = True
+    iso = 0
+    grid_divisions = 50
 
-    def return_all_cell_coords(self):
-        all_cell_coords = []
-        for x in range(self.x_count):
-            for y in range(self.y_count):
-                coords = self.return_cell_coords(x,y)
-                all_cell_coords.append(coords)
+    grid_scale = image_resolution / (grid_divisions - 1)
 
-        return all_cell_coords
+    grid = Grid(grid_scale, grid_divisions, grid_divisions)
 
+    ## Drawing shapes 
+    # Background
+    center_rectangle(image_resolution / 2, image_resolution / 2, image_resolution, image_resolution, f'rgb({73},{73},{71})')
+    ## Circle (for map(p))
+    center_ellipse(image_resolution / 2, image_resolution / 2, radius, f'rgb({68},{204},{255})')
 
+    def draw_edges(grid, img_path):
+
+        edges = march(grid, iso, interpolated)
+
+        for line in edges:
+            p1 = line[0]
+            p2 = line[1]
+            draw.line([p1[0], p1[1], p2[0], p2[1]], fill=f'rgb({255},{255},{255})',width=4)
+        
+        img.save(img_path)
+
+    draw_edges(grid, "marched.png")
 
 if __name__ == "__main__":
-
-    cell = UnitCell("BC")
-
-    lattice = Lattice(cell, 2, 2)
-    lattice.return_cell_coords(0,1)
-    
+    main()
