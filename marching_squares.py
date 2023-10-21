b9d489fe8e2f3c94ff6d368c6877352bc0cec3aa
TristanAntonsen <tristan.antonsen@gmail.com>
2023-10-20 21:24:25 -0500

    removing old march

--- marching_squares.py
+++ marching_squares.py
@@ -14,19 +14,6 @@ class Grid:
         self.y_count = y_count
         self.scale = scale
         self.values = np.zeros((x_count + 1) * (y_count + 1)).reshape(y_count + 1, x_count + 1)
-        self.point_array = np.zeros(x_count * y_count).reshape(y_count, x_count)
-        self.create_points()
-
-    def create_points(self):
-        points = []
-        point_indices = []
-        for x in range(self.x_count):
-            for y in range(self.y_count):
-                points.append([x * self.scale,y * self.scale])
-                point_indices.append([y, x])
-
-        self.points = np.array(points)
-        self.point_indices = np.array(point_indices)
 
 def get_state(a,b,c,d,threshold):
     
@@ -76,65 +63,6 @@ def map_grid(grid: Grid):
             grid.values[j, i] = circle(x, y, 540, 540, 400)
 
 def march(grid: Grid, iso, interpolated=True):
-
-    graph = []
-
-    map_grid(grid)
-
-    for point in grid.points:
-
-        scale = grid.scale
-
-        x = point[0]
-        y = point[1]
-
-        ## Corner points
-        p0 = (x        , y        )
-        p1 = (x + scale, y        )
-        p2 = (x + scale, y + scale)
-        p3 = (x        , y + scale)
-
-        ## values at (corner) points
-        v0 = map(*p0)
-        v1 = map(*p1)
-        v2 = map(*p2)
-        v3 = map(*p3)
-
-        ## Interopolation factors
-        ta = find_lerp_factor(v0, v1, iso)
-        tb = find_lerp_factor(v1, v2, iso)
-        tc = find_lerp_factor(v3, v2, iso) # flip due to sign change
-        td = find_lerp_factor(v0, v3, iso)
-
-        ## edge point locations (interpolated)
-        a = [x + ta * scale , y              ] # 01
-        b = [x + scale      , y + tb * scale ] # 12
-        c = [x + tc * scale , y + scale      ] # 23
-        d = [x              , y + td * scale ] # 30
-
-        ## edge point locations (not interpolated)
-        _a = [x + 0.5 * scale , y               ] # 01
-        _b = [x + scale       , y + 0.5 * scale ] # 12
-        _c = [x + 0.5 * scale , y + scale       ] # 23
-        _d = [x               , y + 0.5 * scale ] # 30
-
-        edge_points = [a,b,c,d]
-
-        if interpolated == False:
-            edge_points = [_a,_b,_c,_d]
-
-        state = get_state(v0, v1, v2, v3, 0)
-        edges = STATES[state]
-        
-
-        for line in edges:
-            p1 = edge_points[line[0]]
-            p2 = edge_points[line[1]]
-            graph.append((p1, p2))
-
-    return graph
-
-def march2(grid: Grid, iso, interpolated=True):
 
     graph = []
 
@@ -218,7 +146,7 @@ def main():
 
     def draw_edges(grid, img_path):
 
-        edges = march2(grid, iso, interpolated)
+        edges = march(grid, iso, interpolated)
         for line in edges:
             p1 = line[0]
             p2 = line[1]
